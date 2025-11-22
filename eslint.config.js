eda95b015c10a3124ee9efecf4b50b8464c80041
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-22 21:49:53 +0100

    initial configuration

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import {defineConfig, globalIgnores} from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  reactRefresh.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-namespace": [
+        "error",
+        {
+          allowDeclarations: true,
+          allowDefinitionFiles: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+    },
+  },
+  {
+    files: ["src/app/**/layout.tsx", "src/app/**/page.tsx"],
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+  },
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
+export default eslintConfig;
